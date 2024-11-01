f03de4d1942a7e4a912f2b14ef6a16a3bcac2cc4
Malnormalulos-git <malnormalulo1@gmail.com>
2024-11-01 23:16:16 +0200

    random rework

--- commands/roll/utils/diceRoller.js
+++ commands/roll/utils/diceRoller.js
@@ -1,5 +1,7 @@
 function rollDice(numOfSides) {
-  return Math.floor(Math.random() * numOfSides) + 1;
+  const randomBuffer = new Uint32Array(1);
+  crypto.getRandomValues(randomBuffer);
+  return (randomBuffer[0] % numOfSides) + 1;
 }
 
 module.exports = { rollDice };
