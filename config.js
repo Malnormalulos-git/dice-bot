79d2869333363de9ca7ae992595a1fcafefc610c
Malnormalulos-git <malnormalulo1@gmail.com>
2024-11-01 22:33:59 +0200

    bot status and activity

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
+const { ActivityType } = require('discord.js');
+
 const config = {
   MAX_DICE_COUNT: 5000,
   MAX_DICE_SIDES: 5000,
   MAX_DISCORD_MESSAGE_LENGTH: 1900,
   MAX_EXPRESSION_LENGTH: 100,
-  ROLL_KEYWORD_SYNONYMS: ['д']
+  ROLL_KEYWORD_SYNONYMS: ['д'],
+  BOT_STATUS: 'dnd',
+  BOT_ACTIVITY: {
+    name: 'Frustrated by a critical failure 🎲😤',
+    type: ActivityType.Custom
+  }
 };
 
 module.exports = config;
