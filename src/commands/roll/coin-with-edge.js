9db80f25617978acee87e580f35fec601a36f795
Malnormalulos-git <malnormalulo1@gmail.com>
2024-12-05 23:29:14 +0200

    coin-with-edge roll command

---
+++ src/commands/roll/coin-with-edge.js
@@ -0,0 +1,22 @@
+﻿const { SlashCommandBuilder } = require('discord.js');
+const { execute } = require('./coin.js');
+const { rollDice } = require("../../business-logic/dice/services/diceRoller");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('coin-with-edge')
+        .setDescription('Flip a coin! (1/1000 chance of edge)'),
+    async execute(interaction) {
+        const result = rollDice(1000);
+
+        if (result === 1000){
+            await interaction.reply({
+                content: `||\`Edge \`||`,
+                files: ['.\\assets\\roll\\coin\\edge.gif']
+            });
+        }
+        else {
+            await execute(interaction);
+        }
+    },
+};
