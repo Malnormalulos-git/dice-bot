f48cce3b700bfab196aaea4b8ce30f11235c6e19
Malnormalulos-git <malnormalulo1@gmail.com>
2024-12-05 23:13:05 +0200

    coin roll command

---
+++ src/commands/roll/coin.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { rollDice } = require('../../business-logic/dice/services/diceRoller');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('coin')
+    .setDescription('Flip a coin!'),
+  async execute(interaction) {
+    const result = rollDice(2);
+
+    await interaction.reply({
+      content: `||\`${result === 1 ? 'Heads' : 'Tails'}\`||`,
+      files: [result === 1 ? '.\\assets\\roll\\coin\\heads.gif' : '.\\assets\\roll\\coin\\tails.gif']
+    });
+  },
+};
