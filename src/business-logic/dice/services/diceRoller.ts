488a5aaa405296a83fd631718dba2ee7def73ac6
Malnormalulos-git <malnormalulo1@gmail.com>
2025-07-02 17:33:26 +0300

    updated rollDice to avoid modulo bias

--- src/business-logic/dice/services/diceRoller.ts
+++ src/business-logic/dice/services/diceRoller.ts
@@ -2,7 +2,12 @@
  * Rolls a dice with the given number of sides
  */
 export function rollDice(numOfSides: number): number {
-    const randomBuffer = new Uint32Array(1);
-    crypto.getRandomValues(randomBuffer);
-    return (randomBuffer[0] % numOfSides) + 1;
+    const array = new Uint32Array(1);
+    const max = Math.floor(0xFFFFFFFF / numOfSides) * numOfSides;
+
+    do {
+        crypto.getRandomValues(array);
+    } while (array[0] >= max);
+
+    return (array[0] % numOfSides) + 1;
 }
